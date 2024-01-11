daf5bfa8c9a4edce1176684e875c3e17d5d11bc4
Girish-3102 <girish.bharadwaj@hyperface.co>
2024-01-11 15:41:29 +0530

    changed employment details in department implementation from List to Set

--- src/main/java/com/example/employeemanagementsystem/services/DepartmentService.java
+++ src/main/java/com/example/employeemanagementsystem/services/DepartmentService.java
@@ -6,11 +6,12 @@ import com.example.employeemanagementsystem.models.Project;
 import com.example.employeemanagementsystem.models.dtos.DepartmentRequest;
 
 import java.util.List;
+import java.util.Set;
 
 public interface DepartmentService {
     List<Department> getAllDepartments();
-    List<Project> getAllProjectsByDepartmentId(Long id);
-    List<Employee> getAllEmployeesByDepartmentId(Long id);
+    Set<Project> getAllProjectsByDepartmentId(Long id);
+    Set<Employee> getAllEmployeesByDepartmentId(Long id);
     Department createDepartment(DepartmentRequest departmentRequest);
     Department getDepartmentById(Long id);
     String deleteDepartmentById(Long id);
